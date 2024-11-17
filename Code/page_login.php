4b5b13af1999fb8df3bf2361bb18ec1fb3054f66
edmond <edmond.berne6@gmail.com>
2024-11-17 22:18:58 +0100

    modif mineure page connexion (regle pb de proportion)

--- Code/page_login.php
+++ Code/page_login.php
@@ -4,67 +4,53 @@
 
     <title>Page de connexion</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="CSS/page_login.css">
+    <link rel="stylesheet" type="text/css" href="CSS/index.css">
 
 </head>
 
 <body>
 
-    <div id = "global">
-        <!-- division pour le fond de la page de connexion -->
-        <div id = "fond">
-            <img src="Ressources/Images/background_login_signin.png" alt="fond">
-        </div id = "fond">
+    <!-- division pour le fond de la page de connexion -->
+    <img src="Ressources/Images/background_login_signin.png" alt="fond" id="fond">
 
-        <!-- Division pour les attributs de la page devant le fond de l'arrière plan -->
-        <div id = "premier_plan">
-            <!-- division pour le fond du 1er plan -->
-            <div id = fond_1er_plan>
-                <img src="Ressources/Images/foreground_login_signin.png" alt="fond_1er_plan">
-            </div id = fond_1er_plan>
-        </div id = "premier_plan">
+    <!-- division pour le fond du 1er plan -->
 
-        <!-- division pour les élements du 1er plan (logo et formulaire) -->
-        <div id = "element_1er_plan">
-            <!-- division pour le logo du 1er plan -->
-            <div id = "logo">
-                <img src="Ressources/Images/logo_login_signin.png" alt="logo">
-            </div id = "logo">
+    <!-- division pour les élements du 1er plan (logo et formulaire) -->
+    <div id="element_1er_plan">
+        <!-- division pour le logo du 1er plan -->
+        <img src="Ressources/Images/logo_login_signin.png" alt="logo" id="logo">
 
-            <!-- division pour le formulaire de connexion -->
-            <div id = "interface_connexion">
-                <div id = "title"> <h1>Login</h1> </div>
+        <!-- division pour le formulaire de connexion -->
+        <div id = "interface_connexion">
+            <h1 id="title">Login</h1>
 
-                <!-- Division pour contenir les champs à remplir pour la connexion -->
-                <div id="texte_connexion">
-                    <div class="champs">
-                        <form action="" method="post">
-                            <label for="identifiant">Email</label>
-                            <input type="text" id="identifiant" name="identifiant" required>
-                            <br><br>
-                            <label for="mdp">Password</label>
-                            <input type="password" id="mdp" name="mdp" required>
-                            <a href="#" id="forgot_password">Forgot your password?</a>
-                            <br><br>
-                            <div id="bouton_de_confirmation">
-                                <button type="submit" class="button">Connect</button>
-                                <button type="button" class="button">Back</button>
-                            </div id = "bouton_de_confirmation" >
-                        </form>
-                    </div class = 'champs'>
-                </div id="texte_connexion">
+            <!-- Division pour contenir les champs à remplir pour la connexion -->
+            <form action="" method="post">
 
-            </div id = "interface_connexion">
+                <div class="input_info">
+                    <label for="identifiant">Email</label>
+                    <input type="text" id="identifiant" name="identifiant" required/>
+                </div>
 
-        </div id = "element_1er_plan">
+                <div class="input_info">
+                    <label for="mdp">Password</label>
+                    <input type="password" id="mdp" name="mdp" required/>
+                    <a href="#" id="forgot_password">Forgot your password?</a>
+                </div>
 
-    </div id = "global">
+                <div class="buttons">
+                    <button type="submit">Connect</button>
+                    <button type="button">Back</button>
+                </div>
+            </form>
 
+        </div>
 
+    </div>
 
 </body>
-
-<?php
+ 
+<!--<?php
 session_start();
 
 if (isset($_POST["identifiant"]) && isset($_POST["mail"]) && isset($_POST["password"])) {
@@ -99,10 +85,10 @@ if (isset($_POST["identifiant"]) && isset($_POST["mail"]) && isset($_POST["passw
             echo "Mot de passe incorrect.";
     } else // Si aucun résultat n'est trouvé, afficher un message d'erreur
         echo "Identifiant ou email incorrect.";
-    
+
     // Fermer le curseur
     $bdd->closeStatement($res);
     $bdd->closeBD($bdd);
 }
 session_abort();
-?>
+?> -->
