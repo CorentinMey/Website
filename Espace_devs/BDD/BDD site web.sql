489d8303d83077c89230daf0eb10034ab3d56ced
edmond <edmond.berne6@gmail.com>
2024-11-21 09:31:03 +0100

    update BDD

--- Espace_devs/BDD/BDD site web.sql
+++ Espace_devs/BDD/BDD site web.sql
@@ -11,8 +11,7 @@ CREATE TABLE `UTILISATEUR` (
 );
 
 CREATE TABLE `ENTREPRISE` (
-  `ID_entreprise` integer PRIMARY KEY,
-  `siret` integer,
+  `siret` integer PRIMARY KEY,
   `ville` varchar(255)
 );
 
@@ -21,6 +20,7 @@ CREATE TABLE `RESULTAT` (
   `ID_patient` integer,
   `is_accepte` bool,
   `is_patient_exclus` bool,
+  `phase` integer,
   `traitement` varchar(255),
   `dose` float,
   `effet_secondaire` varchar(255),
@@ -35,28 +35,26 @@ CREATE TABLE `ESSAI` (
   `date_debut` date,
   `date_fin` date,
   `description` varchar(255),
-  `nombre_patient_ideal` integer,
   `molecule_test` varchar(255),
   `dosage_test` float,
   `molecule_ref` varchar(255),
   `dosage_ref` float,
   `placebo_nom` varchar(255),
-  `is_debute` bool
+  `a_debute` bool
 );
 
 CREATE TABLE `ESSAI_MEDECIN` (
   `ID_medecin` integer,
   `ID_essai` integer,
   `is_accepte` bool,
-  `is_from_company` bool,
+  `est_de_company` bool,
   PRIMARY KEY (`ID_medecin`, `ID_essai`)
 );
 
 CREATE TABLE `MEDECIN` (
-  `ID_medecin` integer PRIMARY KEY,
+  `numero_ordre` integer PRIMARY KEY,
   `domaine` varchar(255),
-  `hopital` varchar(255),
-  `numero_ordre` integer
+  `hopital` varchar(255)
 );
 
 CREATE TABLE `PHASE` (
@@ -72,19 +70,19 @@ ALTER TABLE `PHASE` ADD FOREIGN KEY (`ID_phase`) REFERENCES `ESSAI` (`ID_phase`)
 
 ALTER TABLE `PHASE` ADD FOREIGN KEY (`ID_essai`) REFERENCES `ESSAI` (`ID_essai`);
 
-ALTER TABLE `MEDECIN` ADD FOREIGN KEY (`ID_medecin`) REFERENCES `UTILISATEUR` (`ID_User`);
+ALTER TABLE `MEDECIN` ADD FOREIGN KEY (`numero_ordre`) REFERENCES `UTILISATEUR` (`ID_User`);
 
 ALTER TABLE `ESSAI` ADD FOREIGN KEY (`ID_essai`) REFERENCES `RESULTAT` (`ID_essai`);
 
 ALTER TABLE `ESSAI_MEDECIN` ADD FOREIGN KEY (`ID_essai`) REFERENCES `ESSAI` (`ID_essai`);
 
-ALTER TABLE `ENTREPRISE` ADD FOREIGN KEY (`ID_entreprise`) REFERENCES `UTILISATEUR` (`ID_User`);
+ALTER TABLE `ENTREPRISE` ADD FOREIGN KEY (`siret`) REFERENCES `UTILISATEUR` (`ID_User`);
 
-ALTER TABLE `ESSAI_MEDECIN` ADD FOREIGN KEY (`ID_medecin`) REFERENCES `MEDECIN` (`ID_medecin`);
+ALTER TABLE `ESSAI_MEDECIN` ADD FOREIGN KEY (`ID_medecin`) REFERENCES `MEDECIN` (`numero_ordre`);
 
 ALTER TABLE `RESULTAT` ADD FOREIGN KEY (`ID_patient`) REFERENCES `UTILISATEUR` (`ID_User`);
 
-ALTER TABLE `ESSAI` ADD FOREIGN KEY (`ID_entreprise_ref`) REFERENCES `ENTREPRISE` (`ID_entreprise`);
+ALTER TABLE `ESSAI` ADD FOREIGN KEY (`ID_entreprise_ref`) REFERENCES `ENTREPRISE` (`siret`);
 
 ALTER TABLE `PHASE` ADD FOREIGN KEY (`ID_essai`) REFERENCES `RESULTAT` (`ID_essai`);
 
