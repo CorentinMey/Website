ef925800872902da5c37b8685678ffeede26cfc5
Simon De Montardy <simon2mon@gmail.com>
2024-11-22 10:44:43 +0100

    affichage page admin

---
+++ Code/page_admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+
+    <title>Page de connexion</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="CSS/page_admin.css">
+
+</head>
+
+<body>
+    <h1>Welcome admin</h1>
+    <img src="Ressources/Images/background_admin.jpg" alt="fond" id="fond">
+</body>
