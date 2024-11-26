714eb5eabba8a03599d08f7ed63ead1c9dc40674
Simon De Montardy <simon2mon@gmail.com>
2024-11-26 15:34:51 +0100

    frontendadmin

--- Code/src/page/page_admin.php
+++ Code/src/page/page_admin.php
@@ -4,25 +4,26 @@
 
     <title>Page de connexion</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../CSS/page_admin.css">
     <link rel="stylesheet" type="text/css" href="../CSS/global.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/page_admin.css">
 
 </head>
 
 <body>
-    <img src="Ressources/Images/background_admin2.jpg" alt="fond" id="fond">
+    <img src="../Ressources/Images/background_admin2.jpg" alt="fond" id="fond">
     <div id="bandeau_top">
-        <h1>cheh</h1>
         <h1>Admin page</h1>
-        <img src="Ressources/Images/profil.png" alt="profil" id="profil">
+        <img src="../Ressources/Images/profil.png" alt="profil" id="profil">
 
 
     </div>
     <div class="button-container" id="content_button">
-        <button class="btn" id="button1" onclick="window.location.href='page1.html';">Bouton 1</button>
-        <button class="btn" id="button2" onclick="window.location.href='page2.html';">Bouton 2</button>
-        <button class="btn" id="button3" onclick="window.location.href='page3.html';">Bouton 3</button>
-        <button class="btn" id="button4" onclick="window.location.href='page4.html';">Bouton 4</button>
-        <button class="btn" id="button5" onclick="window.location.href='page5.html';">Bouton 5</button>
-    </div>
+        <button class="btn" id="button1" onclick="window.location.href='page1.html';">User List</button>
+        <button class="btn" id="button2" onclick="window.location.href='page2.html';">Doc List</button>
+        <button class="btn" id="button3" onclick="window.location.href='page2.html';">Company List</button>
+        <button class="btn" id="button4" onclick="window.location.href='page2.html';">Clinical assay List</button>
+        <button class="confirmation" id="button5" onclick="window.location.href='page2.html';">Required Confirmation for inscription
+            <span class="pastille" id="notifCount">0</span>
+        </button>
+        
 </body>
