6727331562780c1ba41d62aa0aa4e94c7f58eedd
Simon De Montardy <simon2mon@gmail.com>
2024-12-04 15:35:19 +0100

    avancement accueil page

--- Code/src/page/page_accueil.php
+++ Code/src/page/page_accueil.php
@@ -16,16 +16,32 @@
             <h2>A new way for health research</h2>
         </div>
     </div>
-
+    
     <div id="bandeau_top">
         <h1>WELCOME</h1>
         <form method="POST" action="accueil.php" id="bande_welcome">
-            <button type="submit" class="btn" name="show_list_user" value="1" id="button1">SIGN IN</button>
-            <button type="submit" class="btn" name="show_list_doc" value="1" id="button2">LOG IN </button>
+            <button type="submit" class="button" name="show_list_user" value="1" id="button1">SIGN IN</button>
+            <button type="submit" class="button" name="show_list_doc" value="1" id="button2">LOG IN </button>
         </form>
     </div>
+    <div id="ma_div">
+        <!-- <img src="../Ressources/Images/background_accueil.png" alt="fond" id="fond"> -->
+        <h1 id="bienvenue">Welcome on <br>MedExplorer</h1>
+        <h2 id="ready" >ready to start your trial ?</h2>
+    </div>
+    <br>
+    <br>
+    <br>    
 
-
+    <div>
+        <h1 id="why">Why choosing us ?<br></h1>
+    <div id="grid_container">
+        <div class="box">Box 1</div>
+        <div class="box">Box 2</div>
+        <div class="box">Box 3</div>
+        <div class="box">Box 4</div>
+    </div>
+    </div>
 
 <?php
 
