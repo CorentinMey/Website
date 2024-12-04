52d1c0baf8c252da445d42ee1cfe9e58527527cb
Simon De Montardy <simon2mon@gmail.com>
2024-12-04 11:50:37 +0100

    crea accueil page

--- Code/src/page/page_accueil.php
+++ Code/src/page/page_accueil.php
@@ -1,4 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome page</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/global.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/page_accueil.css">
+    
+</head>
+<body>
+    <div id = "en-tete">
+        <img src = "../Ressources/Images/logo_medexplorer.png" alt = "logo_med_explorer" id = logo_page>
+        <div id = "en-tete_text">
+            <h1>MedExplorer</h1>
+            <h2>A new way for health research</h2>
+        </div>
+    </div>
+
+    <div id="bandeau_top">
+        <h1>WELCOME</h1>
+        <form method="POST" action="accueil.php" id="bande_welcome">
+            <button type="submit" class="btn" name="show_list_user" value="1" id="button1">SIGN IN</button>
+            <button type="submit" class="btn" name="show_list_doc" value="1" id="button2">LOG IN </button>
+        </form>
+    </div>
+
+
+
 <?php
-echo "Bien ouej tu es inscrit";
 
 ?>
+<body>
