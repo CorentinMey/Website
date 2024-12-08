df792c66ec2a12b9b23f688864dc6e22d373c753
edmond <edmond.berne6@gmail.com>
2024-12-08 16:13:37 +0100

    ajout de la page d'historique général

---
+++ Code/src/page/page_historique.php
@@ -0,0 +1,61 @@
+<?php
+include_once("../back_php/Query.php");
+include_once("../back_php/Affichage_patient.php");
+include_once("../back_php/Patient.php");
+session_start();
+
+// TODO ajouter les autres utilisateurs
+if (!isset($_SESSION["patient"])) { // si quelqu'un essaie d'accéder à la page sans être connecté on le redirige vers la page de connexion
+    header("Location: page_login.php");
+    exit;
+}
+
+$bdd = new Query("siteweb");
+?>
+
+
+<!DOCTYPE html>
+
+<head>
+    <title>Page du patient</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../CSS/page_historique.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/global.css">
+</head>
+
+
+<body>
+    <div id = "en-tete">
+        <img src = "../Ressources/Images/logo_medexplorer.png" alt = "logo_med_explorer" id = logo_page>
+        <div id = "en-tete_text">
+            <h1>MedExplorer</h1>
+            <h2>A new way for health research</h2>
+        </div>
+    </div>
+
+    <div id="banderolle">
+        
+        <!-- div pour le log de l'historique et son bouton -->
+        <div id = "logo_container_hist">
+            <a href = "page_deco.php">
+                <img id = "logo_historic" src = "../Ressources/Images/logo_historic.png" alt = "Historic button">
+                <div id = "tooltip_hist">Historic</div>
+            </a>
+        </div>
+
+        <!-- titre de la banderolle -->
+        <h1 id="title">My account</h1>
+
+        <!-- div pour le logo de deconnexion et son bouton -->
+        <div id="logo_container">
+            <!-- mettre logo dans une balsie ref a pour rediriger -->
+            <a href="page_deco.php">
+                <img id="logo_account" src="../Ressources/Images/account.png" alt="Account Logo">
+                <div id="tooltip">Disconnect</div>
+            </a>
+        </div>
+    </div>
+
+    <img src = "../Ressources/Images/test_banderolle.webp" alt = "banderolle" id = "banderolle_img">
+
+</body>
